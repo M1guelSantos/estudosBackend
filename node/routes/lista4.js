2cb289856f7e585e25a860e29810b95f9ea8c745
unknown <22114290034@ACADEMICO.IESB.BR>
2024-03-13 21:57:36 -0300

    aula 13_03

---
+++ node/routes/lista4.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router()
+
+router.post("/ex1", (req, res) => {
+    const numeros = req.body
+    let soma = 0
+    for(let n of numeros){
+        soma += n
+    }
+    const media = soma / numeros.length
+    res.json(media)
+})
+
+
+
+
+module.exports = router
