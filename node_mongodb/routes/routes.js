591c3318a8f216f8ba97f359b501200ee1ae4842
unknown <22114290034@ACADEMICO.IESB.BR>
2024-03-22 21:32:11 -0300

    aula 22_03

--- node_mongodb/routes/routes.js
+++ node_mongodb/routes/routes.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router()
+const Produto = require("../models/Produto")
+
 
 router.get("/", (req, res)=>{
     res.json({})
 })
+
+router.get("/produtos", async (req, res)=>{
+    res.json(await Produto.find())
+    
+})
+router.post("/produtos", async (req, res)=>{
+    res.json(await Produto.create(req.body))
+})
+
+router.get("/produtos/:id", async (req, res)=>{
+    res.json(await Produto.findById(req.params.id))
+})
+
+router.delete("/produtos/:id", async (req, res)=>{
+    res.json(await Produto.findByIdAndDelete(req.params.id))
+})
+
+router.put("/produtos/:id", async (req, res)=>{
+    res.json(await Produto.findByIdAndUpdate(req.params.id, req.body))
+})
+
 
 
 module.exports = router
